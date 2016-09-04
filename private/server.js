bd425ac1ef5fa9a14d4990f4df2827458f75bda7
James Lemire <webdev.jameslemire@gmail.com>
2016-09-04 13:33:08 -0600

    Basic Server Configuration Completed

--- private/server.js
+++ private/server.js
@@ -6,9 +6,16 @@ var massive = require('massive');
 var connectionString = "postgres://postgres:password@localhost/fakebook"
 // var connectionString = "postgres://jameslemire@localhost/fakebook";
 // var connectionString = "postgres://michaellecke@localhost/fakebook";
+// var connectionString = "postgres://LonnieMcGill@localhost/fakebook";
+// Brandon's connectionString here
 var massiveInstance = massive.connectSync({connectionString : connectionString});
+var config = require('./config.js');
 
-var app = express();
-//test
+var app = module.exports = express();
+app.set('db', massiveInstance);
+
+app.use(cors(config.corsOptions));
+app.use(bodyParser.json());
+app.use(express.static('../public'));
 
 app.listen(3000, function() { console.log('Server initiated on port 3000'); })
