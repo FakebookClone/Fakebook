5569a5eeb2879787c4fdb98521d7341d69beb02c
James Lemire <webdev.jameslemire@gmail.com>
2016-09-04 18:24:38 -0600

    Better Server File, thanks Michael

--- private/server.js
+++ private/server.js
@@ -2,14 +2,9 @@ var aws = require('aws-sdk');
 var cors = require('cors');
 var express = require('express');
 var bodyParser = require('body-parser');
-var massive = require('massive');
-// var connectionString = "postgres://postgres:password@localhost/fakebook"
-var connectionString = "postgres://jameslemire@localhost/fakebook";
-// var connectionString = "postgres://michaellecke@localhost/fakebook";
-// var connectionString = "postgres://LonnieMcGill@localhost/fakebook";
-// Brandon's connectionString here
-var massiveInstance = massive.connectSync({connectionString : connectionString});
 var config = require('./config.js');
+var massive = require('massive');
+var massiveInstance = massive.connectSync({connectionString : config.connectionString});
 
 var app = module.exports = express();
 app.set('db', massiveInstance);
