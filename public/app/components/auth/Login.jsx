fd210c49c9332c4bd19d97d01ffa34bc6b19e830
mlecke <mlecke@beespace.com>
2016-09-05 14:51:44 -0600

    bleu header

--- public/app/components/auth/Login.jsx
+++ public/app/components/auth/Login.jsx
@@ -21,13 +21,11 @@ export default class Login extends React.Component {
 
 	render() {
 		return (
-			<div>
-				{this.state.user && this.state.user.status !== 'unknown'
-					? <div>
-							<img src={this.state.user.picture.data.url}/>
-							<button className="login-button" onClick={this.logout.bind(this)}>Logout</button>
-						</div>
-					: <div><FacebookLogin appId="145051979269944" autoLoad={false} fields="name,first_name,last_name,email,picture,cover" cssClass="login-button" textButton="Log in" callback={this.responseFacebook.bind(this)}/>
+			<div className="login-main-wrapper">
+				<div>
+					<div className="login-header-container">
+          <img src="" alt="facebook logo"/>
+          <FacebookLogin appId="145051979269944" autoLoad={false} fields="name,first_name,last_name,email,picture,cover" cssClass="login-button" textButton="Log in" callback={this.responseFacebook.bind(this)}/>
 						<div>
 							<label htmlFor="email">Email or Phone</label>
 							<input className="inputtext" type="text" name="email" tabIndex="1"/>
@@ -36,15 +34,10 @@ export default class Login extends React.Component {
 							<label htmlFor="pass">Password</label>
 							<input className="inputtext" type="password" name="pass" tabIndex="2"/>
 						</div>
-            <a href="#">Forgot account?</a>
-					</div>}
+						<a href="#">Forgot account?</a>
+					</div>
+				</div>
 			</div>
 		)
 	}
-
-	logout() {
-		localStorage.clear();
-		this.setState({user: null})
-	}
-
 }
