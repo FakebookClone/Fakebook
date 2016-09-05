ce220866bc799f9da010d195506e919436024f83
James Lemire <webdev.jameslemire@gmail.com>
2016-09-05 17:53:05 -0600

    Make header global

--- public/app/components/home/Home.jsx
+++ public/app/components/home/Home.jsx
@@ -1,46 +1,17 @@
 import React from 'react';
+import GlobalHeader from '../global/GlobalHeader.jsx';
 
-var images = './images/main/';
 require('../../../stylesheets/components/Home.scss');
 
 export default class Home extends React.Component {
   constructor() {
     super()
-    this.state = { dimmerVisible: false };
   }
 
   render() {
     return (
       <div>
-        {this.state.dimmerVisible
-          ? <div className="dimmer"></div>
-          : null
-        }
-        <div className="home-header">
-          <img onClick={this.toggleDimmer.bind(this)} className="same-line" src={images + 'facebook_logo.jpg'} />
-          <input className="same-line home-header-find-friends-input" placeholder="Find Friends" />
-          <button><i className="fa fa-search same-line home-search-button"></i></button>
-          <div className="same-line home-header-nav-container">
-            <img className="same-line home-header-profile-picture" src="broken-img" />
-            <p className="same-line">name</p>
-          </div>
-          <div className="same-line home-header-nav-container">
-            <p className="same-line">Home</p>
-          </div>
-          <div className="same-line home-header-nav-container">
-            <p className="same-line">Find Friends</p>
-          </div>
-          <div className="same-line home-header-nav-container">
-            <img className="home-header-nav-image same-line" src={images + 'friend_request_not_selected.png'} />
-            <img className="home-header-nav-image same-line" src={images + 'messages_not_selected.png'} />
-            <img className="home-header-nav-image same-line" src={images + 'notifications_not_selected.png'} />
-          </div>
-          <div className="same-line home-header-nav-container">
-            <img className="home-header-nav-image-small same-line" src={images + 'lock_not_selected.png'} />
-            <img className="home-header-nav-image-smallest same-line" src={images + 'down_arrow_not_selected.png'} />
-          </div>
-        </div>
-
+        <GlobalHeader />
         <div className="home-main-content-wrapper">
           <div className="home-left-column">
 
