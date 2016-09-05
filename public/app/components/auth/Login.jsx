2464d635d977b30538075b71b59a4919926fb3b6
mlecke <mlecke@beespace.com>
2016-09-05 12:04:40 -0600

    red styled button, added logic to check if local storage facebook_user status is unknown

--- public/app/components/auth/Login.jsx
+++ public/app/components/auth/Login.jsx
@@ -4,42 +4,47 @@ import FacebookLogin from 'react-facebook-login';
 require('../../../stylesheets/login.scss');
 
 export default class Login extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { user: JSON.parse(localStorage.getItem('fakebook_user')) }
-  }
+	constructor(props) {
+		super(props)
+		this.state = {
+			user: JSON.parse(localStorage.getItem('fakebook_user'))
+		}
+	}
 
-  responseFacebook(response) {
-    console.log(response);
-    localStorage.setItem('fakebook_user', JSON.stringify(response));
-    this.setState({ user: JSON.parse(localStorage.getItem('fakebook_user')) });
-  }
+	responseFacebook(response) {
+		console.log(response);
+		localStorage.setItem('fakebook_user', JSON.stringify(response));
+		this.setState({
+			user: JSON.parse(localStorage.getItem('fakebook_user'))
+		});
+	}
 
-  render() {
-    return (
-      <div>
-        {this.state.user
-          ? <div>
-              <img src={this.state.user.picture.data.url} />
-              <button onClick={this.logout.bind(this)}>Logout</button>
-            </div>
-          : <FacebookLogin
-              appId="145051979269944"
-              autoLoad={false}
-              fields="name,first_name,last_name,email,picture,cover"
-              cssClass="test"
-              textButton="Login"
-              callback={this.responseFacebook.bind(this)}
-            />
-        }
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div>
+				{this.state.user && this.state.user.status !== 'unknown'
+					? <div>
+							<img src={this.state.user.picture.data.url}/>
+							<button className="login-button" onClick={this.logout.bind(this)}>Logout</button>
+						</div>
+					: <div><FacebookLogin appId="145051979269944" autoLoad={false} fields="name,first_name,last_name,email,picture,cover" cssClass="login-button" textButton="Log in" callback={this.responseFacebook.bind(this)}/>
+						<div>
+							<label htmlFor="email">Email or Phone</label>
+							<input className="inputtext" type="text" name="email" tabIndex="1"/>
+						</div>
+						<div>
+							<label htmlFor="pass">Password</label>
+							<input className="inputtext" type="password" name="pass" tabIndex="2"/>
+						</div>
+            <a href="#">Forgot account?</a>
+					</div>}
+			</div>
+		)
+	}
 
-  logout() {
-    console.log('hit');
-    localStorage.clear();
-    this.setState({ user: null })
-  }
+	logout() {
+		localStorage.clear();
+		this.setState({user: null})
+	}
 
 }
