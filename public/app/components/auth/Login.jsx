afd256708e829ea111c9410ca39d525163cf0645
mlecke <mlecke@beespace.com>
2016-09-05 16:21:23 -0600

    finished login screen

--- public/app/components/auth/Login.jsx
+++ public/app/components/auth/Login.jsx
@@ -1,42 +1,24 @@
 import React from 'react';
-import FacebookLogin from 'react-facebook-login';
+import LoginHeaderForm from './LoginHeaderForm.jsx';
+import LoginMainSection from './LoginMainSection.jsx';
 
 require('../../../stylesheets/components/login.scss');
 
 export default class Login extends React.Component {
 	constructor(props) {
 		super(props)
-		this.state = {
-			user: JSON.parse(localStorage.getItem('fakebook_user'))
-		}
-	}
-
-	responseFacebook(response) {
-		console.log(response);
-		localStorage.setItem('fakebook_user', JSON.stringify(response));
-		this.setState({
-			user: JSON.parse(localStorage.getItem('fakebook_user'))
-		});
 	}
 
 	render() {
 		return (
 			<div className="login-main-wrapper">
-				<div>
 					<div className="login-header-container">
-          <img src="" alt="facebook logo"/>
-          <FacebookLogin appId="145051979269944" autoLoad={false} fields="name,first_name,last_name,email,picture,cover" cssClass="login-button" textButton="Log in" callback={this.responseFacebook.bind(this)}/>
-						<div>
-							<label htmlFor="email">Email or Phone</label>
-							<input className="inputtext" type="text" name="email" tabIndex="1"/>
-						</div>
-						<div>
-							<label htmlFor="pass">Password</label>
-							<input className="inputtext" type="password" name="pass" tabIndex="2"/>
-						</div>
-						<a href="#">Forgot account?</a>
+          <img className="login-logo" src="../../../images/login/login-logo.png" alt="facebook logo"/>
+          <LoginHeaderForm />
 					</div>
-				</div>
+          <div className="login-main-section">
+            <LoginMainSection />
+          </div>
 			</div>
 		)
 	}
