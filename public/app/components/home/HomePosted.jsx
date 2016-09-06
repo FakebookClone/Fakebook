0feba2346df8355ea3fe0012e76753a0a328394f
James Lemire <webdev.jameslemire@gmail.com>
2016-09-06 14:49:56 -0600

    Hide No posts to show if there is a post

--- public/app/components/home/HomePosted.jsx
+++ public/app/components/home/HomePosted.jsx
@@ -8,9 +8,14 @@ export default class HomePosted extends React.Component {
   render() {
     return (
       <div className="home-center-posted-container">
-        <img src="#" />
-        <p>No posts to show</p>
-        <button>Find Friends</button>
+        {this.props.posts.length === 0
+          ? <div>
+              <img src="#" />
+              <p>No posts to show</p>
+              <button>Find Friends</button>
+            </div>
+          : null
+        }
         {this.props.posts.map( (value) => {
           return (
             <div key={'post_container_' + value.post_id}>
