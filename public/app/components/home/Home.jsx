bab47629bcf1001732d85f1d4cee7efeb568abac
James Lemire <webdev.jameslemire@gmail.com>
2016-09-06 10:01:45 -0600

    Force user to be logged in to get to home

--- public/app/components/home/Home.jsx
+++ public/app/components/home/Home.jsx
@@ -3,12 +3,16 @@ import GlobalHeader from '../global/GlobalHeader.jsx';
 import HomeLeft from './HomeLeft.jsx';
 import HomeCenter from './HomeCenter.jsx';
 import HomeRight from './HomeRight.jsx';
+import { browserHistory } from 'react-router';
 
 require('../../../stylesheets/components/Home.scss');
 
 export default class Home extends React.Component {
   constructor() {
     super()
+    if( !(localStorage.getItem('fakebook_user')) ) {
+      browserHistory.push('/');
+    }
   }
 
   render() {
