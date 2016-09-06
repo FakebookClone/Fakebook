c984aef4cd83934ba84dc521ca22a73314dedffb
mlecke <mlecke@beespace.com>
2016-09-05 21:50:56 -0600

    working on sprites

--- public/app/components/home/HomeLeft.jsx
+++ public/app/components/home/HomeLeft.jsx
@@ -1,42 +1,49 @@
 import React from 'react';
 
 export default class HomeLeft extends React.Component {
-  constructor() {
-    super()
-  }
+	constructor() {
+		super()
+	}
 
-  render() {
-    return (
-      <div className="home-left-column">
-        <ul>
-          <li><img src="#" />Profile Name</li>
-          <li><img src="#" />Edit Profile</li>
-          <h4>FAVORITES</h4>
-          <li><img src="#" />News Feed</li>
-          <li><img src="#" />Welcome</li>
-          <li><img src="#" />Messages</li>
-          <li><img src="#" />Events</li>
-          <h4>APPS</h4>
-          <li><img src="#" />Live Video</li>
-          <li><img src="#" />Games</li>
-          <li><img src="#" />Find Friends</li>
-          <li><img src="#" />Photos</li>
-          <li><img src="#" />Suggest Edits</li>
-          <li><img src="#" />Pokes</li>
-          <h4>PAGES</h4>
-          <li><img src="#" />Pages Feed</li>
-          <li><img src="#" />Like Pages</li>
-          <li><img src="#" />Create Pages</li>
-          <h4>FUNDRAISERS</h4>
-          <li><img src="#" />Create Fundraiser</li>
-          <li><img src="#" />Find Fundraiser</li>
-          <h4>GROUPS</h4>
-          <li><img src="#" />Discover Groups</li>
-          <li><img src="#" />Create Groups</li>
-          <h4>EVENTS</h4>
-          <li><img src="#" />Create Event</li>
-        </ul>
-      </div>
-    )
-  }
+	render() {
+		var spriteStyle = {
+			width: '16px',
+			height: '16px',
+			background: 'url(./images/home/home-left-column-sprites.png)0px 32px'
+		}
+		return (
+			<div className="home-left-column">
+
+				<ul>
+					<li><img src="#"/>Profile Name</li>
+					<li><img src="#"/>Edit Profile</li>
+					<h4>FAVORITES</h4>
+					<li><img src="#"/>News Feed</li>
+					<li><img src="#"/>Welcome</li>
+					<li><img src="#"/>Messages</li>
+					<li><img src="#"/>Events</li>
+					<h4>APPS</h4>
+					<li><img src="#"/>Live Video</li>
+					<li><img src="#"/>Games</li>
+					<li><img src="#"/>Find Friends</li>
+					<li><img src="#"/>Photos</li>
+					<li><img src="#"/>Suggest Edits</li>
+					<li><img src="#"/>Pokes</li>
+					<h4>PAGES</h4>
+					<li><img src="#"/>Pages Feed</li>
+					<li><img src="#"/>Like Pages</li>
+					<li><img src="#"/>Create Pages</li>
+					<h4>FUNDRAISERS</h4>
+					<li><img src="#"/>Create Fundraiser</li>
+					<li><img src="#"/>Find Fundraiser</li>
+					<h4>GROUPS</h4>
+					<li><img src="#"/>Discover Groups</li>
+					<li><img src="#"/>Create Groups</li>
+					<h4>EVENTS</h4>
+					<li><img src="#"/>Create Event</li>
+				</ul>
+        <div style={spriteStyle}></div>
+			</div>
+		)
+	}
 }
