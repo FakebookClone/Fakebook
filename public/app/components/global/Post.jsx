6e75200462f3996ae4a4d14c4ef97b141dfec4e8
Lonnie McGill <mcgizmo_26@yahoo.com>
2016-09-08 12:26:22 -0600

    getting updates

--- public/app/components/global/Post.jsx
+++ public/app/components/global/Post.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
 import Axios from 'axios';
 import Comment from './Comment.jsx';
-var imageshome= './images/home/';
+var imageshome = './images/home/';
 
 require('../../../stylesheets/components/global/Post.scss');
 
-
 export default class Posts extends React.Component {
-  constructor() {
-    super()
-    this.state = { postedComments: [], comment: '', likes: [] };
-  }
+	constructor() {
+		super()
+		this.state = {
+			postedComments: [],
+			comment: '',
+			likes: []
+		};
+	}
 
-  componentWillMount() {
-  	Axios.get(`/api/comments/${this.props.post.post_id}`).then( r => {
-  		this.setState({ postedComments: r.data });
-  	});
-    Axios.get(`/api/likes/post/${this.props.post.post_id}`).then( r => {
-      this.setState({ likes: r.data })
-    })
-  }
+	componentWillMount() {
+		Axios.get(`/api/comments/${this.props.post.post_id}`).then(r => {
+			this.setState({postedComments: r.data});
+		});
+		Axios.get(`/api/likes/post/${this.props.post.post_id}`).then(r => {
+			this.setState({likes: r.data})
+		})
+	}
 
 	render() {
 		return (
@@ -33,50 +36,62 @@ export default class Posts extends React.Component {
 					<p className="posted-text">{this.props.post.post_text}</p>
 				</div>
 				<div className="mid-posted-icon-div">
-					<img onClick={this.likePost.bind(this)} src={imageshome + 'gray-like.png'}/>
-					<p>Like</p>
-					<img src={imageshome + 'gray-comment-small.png'}/>
-					<p>Comment</p>
-					<img src={imageshome + 'gray-share-small.png'}/>
-					<p>Share</p>
+
+					<div>
+						<img onClick={this.likePost.bind(this)} src={imageshome + 'gray-like.png'}/>
+						<p>Like</p>
+					</div>
+
+					<div>
+						<img src={imageshome + 'gray-comment-small.png'}/>
+						<p>Comment</p>
+					</div>
+					
+					<div>
+						<img src={imageshome + 'gray-share-small.png'}/>
+						<p>Share</p>
+					</div>
+
 				</div>
 
-        {this.state.likes.length !== 0
-          ? <div>
-              <img src="broken-link" /><p>{this.state.likes.length}</p>
-            </div>
-          : null
-        }
+				{this.state.likes.length !== 0
+					? <div>
+							<img src="broken-link"/>
+							<p>{this.state.likes.length}</p>
+						</div>
+					: null
+}
 
-				{this.state.postedComments.map( (value) => {
-          return (
-            <Comment user={this.props.user} key={'comment_container_' + value.comment_id} comment={value} />
-          )
-        })}
+				{this.state.postedComments.map((value) => {
+					return (<Comment user={this.props.user} key={'comment_container_' + value.comment_id} comment={value}/>)
+				})}
 
-        <input onChange={this.commentCatcher.bind(this)} placeholder="Write a comment..." value={this.state.comment} onKeyDown={this.postComment.bind(this)} />
-        <img src="broken-link" />
-        <img src="broken-link" />
-        <p>Press Enter to post.</p>
-      </div>
-    )
-  }
+				<input onChange={this.commentCatcher.bind(this)} placeholder="Write a comment..." value={this.state.comment} onKeyDown={this.postComment.bind(this)}/>
+				<img src="broken-link"/>
+				<img src="broken-link"/>
+				<p>Press Enter to post.</p>
+			</div>
+		)
+	}
 
-  commentCatcher(e) {
-    this.setState({ comment: e.target.value })
-  }
+	commentCatcher(e) {
+		this.setState({comment: e.target.value})
+	}
 
-  postComment(e) {
-    if( e.keyCode === 13 ) {
-      Axios.post(`/api/comment/${this.props.post.post_id}`, { comment: this.state.comment, profile_id: this.props.user.id }).then( r => {
-        this.setState({ postedComments: r.data, comment: '' })
-      })
-    }
-  }
+	postComment(e) {
+		if (e.keyCode === 13) {
+			Axios.post(`/api/comment/${this.props.post.post_id}`, {
+				comment: this.state.comment,
+				profile_id: this.props.user.id
+			}).then(r => {
+				this.setState({postedComments: r.data, comment: ''})
+			})
+		}
+	}
 
-  likePost() {
-    Axios.post(`/api/like/post/${this.props.post.post_id}`, { profile_id: this.props.user.id }).then( r => {
-      this.setState({ likes: r.data });
-    })
-  }
+	likePost() {
+		Axios.post(`/api/like/post/${this.props.post.post_id}`, {profile_id: this.props.user.id}).then(r => {
+			this.setState({likes: r.data});
+		})
+	}
 }
