71ae3f40830d74945c583a5bf44bff8d2ee0ff39
Brandon Unbedacht <unbedachtb@gmail.com>
2016-09-09 11:56:18 -0600

    finished the profile photos

--- public/app/components/profile/ProfilePhotosGallery.jsx
+++ public/app/components/profile/ProfilePhotosGallery.jsx
@@ -12,19 +12,16 @@ export default class ProfilePhotosGallery extends React.Component {
 			<div className="main-container">
 				<div className="upper-photos-section-wrapper">
 					<div className="photo-image-wrapper">
-					  <img src="/images/profile/photos_24.png"/>
-						<h1 className="text-photos">Photos</h1>
+						<img src="/images/profile/photos_24.png"/>
+						<h1>Photos</h1>
 					</div>
 
-					<div className="create-add-and-video-buttons">
-						<button className="create-album-button"> <img src="/images/profile/gray-plus.png"/>Create Album</button>
+					<div className="create-add-video-and-pencil-buttons">
+						<button className="create-album-button"><img className="gray-plus-image" src="/images/profile/gray-plus.png"/>Create Album</button>
 						<button className="add-photos-button">Add Photos</button>
 						<button className="add-video-button">Add Video</button>
-
-						{/* <div className="pencil-button"> */}
-						<button><img src="/images/profile/gray-pencil.png"/></button>
-						{/* </div> */}
-						</div>
+						<button className="pencil-button"><img className="gray-pencil-image" src="/images/profile/gray-pencil.png"/></button>
+					</div>
 				</div>
 				<div className="lower-section-wrapper">
 					<h1 className="no-photos-to-show">No Photos to Show</h1>
