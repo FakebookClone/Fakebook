873a26cf9b6660a94160a6844926dbfd3ed27917
mlecke <mlecke@beespace.com>
2016-09-10 16:05:44 -0600

    work in progress

--- public/app/components/profile/ProfileIntro.jsx
+++ public/app/components/profile/ProfileIntro.jsx
@@ -3,18 +3,37 @@ import React from 'react';
 require('../../../stylesheets/components/profile/ProfileIntro.scss');
 
 export default class ProfileIntro extends React.Component {
-  constructor () {
-    super()
-  }
+	constructor() {
+		super()
+	}
 
-  render () {
-    return (
-      <div>
-        <img src="/images/profile/blue-plus.png" />
-        <img src="/images/profile/intro-globe.png" />
-        <img src="/images/profile/add-photo.png" />
-        <h1>Intro</h1>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div className="profile-intro-wrapper">
+				<div className="profile-globe-wrapper">
+					<div className="profile-intro-icon">
+						<img src="/images/profile/intro-globe.png"/>
+					</div>
+					<div className="profile-intro-header">Intro</div>
+				</div>
+				<div className="profile-describe-wrapper">
+					<div><img src="/images/profile/blue-plus.png"/></div>
+					<div>Describe Who You Are</div>
+				</div>
+				<div>
+					<div><img src="/images/profile/suitcase-gray.png"/></div>
+					<div>Shameless Yes Man at <a href="#">DevMountain</a></div>
+				</div>
+				<div>
+					<div><img src="/images/profile/house-gray.png"/></div>
+					<div>Lives in Provo, Utah</div>
+				</div>
+				<div>
+					<div><a href="#"><img src="/images/profile/add-photo.png"/></a></div>
+					<div><a href="#">Add Featured Photos</a></div>
+				</div>
+			</div>
+
+		)
+	}
 }
