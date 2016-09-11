b837db7aab8fcec31ab6d6757503ea0e9c041a84
Lonnie McGill <mcgizmo_26@yahoo.com>
2016-09-11 12:41:03 -0600

    pushing style changes

--- public/app/components/home/HomePosted.jsx
+++ public/app/components/home/HomePosted.jsx
@@ -1,31 +1,38 @@
 import React from 'react';
 import Posts from '../global/Post.jsx';
+var imageshome = './images/home/';
 
 require('../../../stylesheets/components/home/HomePosted.scss');
 
 export default class HomePosted extends React.Component {
-  constructor() {
-    super()
-  }
+	constructor() {
+		super()
+	}
 
-  render() {
-    return (
-      <div className= "postedUser">
-        {this.props.posts.length === 0
-          ? <div>
-              <img src="#" />
-              <p>No posts to show</p>
-              <button>Find Friends</button>
-            </div>
-          : null
-        }
-        {this.props.posts.map( (value) => {
-          return (
-            <Posts user={this.props.user} key={'post_component_' + value.post_id} post={value} />
-          )
-        })}
-        <div className="posted-bottom-padding"></div>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div className="postedUser">
+				{this.props.posts.length === 0
+					? <div className="center-section-div">
+							<div className="no-post-image-div">
+                <div>
+                  <img src={imageshome + 'home-post.png'}/>
+                </div>
+							</div>
+							<div className="no-post-text-div">
+								<p>No posts to show</p>
+							</div>
+							<div className="no-post-button-div">
+								<button>Find Friends</button>
+							</div>
+						</div>
+					: null
+}
+				{this.props.posts.map((value) => {
+					return (<Posts user={this.props.user} key={'post_component_' + value.post_id} post={value}/>)
+				})}
+				<div className="posted-bottom-padding"></div>
+			</div>
+		)
+	}
 }
