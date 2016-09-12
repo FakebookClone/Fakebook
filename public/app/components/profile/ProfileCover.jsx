47c2cc9ba5a7ca47826d65319c7d701361c8bfd6
mlecke <mlecke@beespace.com>
2016-09-11 18:08:58 -0600

    buttons and name placed

--- public/app/components/profile/ProfileCover.jsx
+++ public/app/components/profile/ProfileCover.jsx
@@ -17,16 +17,17 @@ export default class ProfileCover extends React.Component {
 					<div className="add-cover-text">
 						<p>Add Cover Photo</p>
 					</div>
-
 				</div>
-				<div>
-					<h1 className="profile-name">Facebook Clone</h1>
-				</div>
-				<div>
-					<button>Update Info</button>
-					<button>View Activity Log</button>
-					<button><img src="/images/profile/gray-elipsis.png" /></button>
 
+				<div className="name-wrapper">
+					<div>
+						<h1 className="profile-name">Facebook Clone</h1>
+					</div>
+					<div className="profile-name-buttons">
+						<button className="profile-update-info">Update Info</button>
+						<button className="profile-activity-log">View Activity Log</button>
+						<button className="profile-cover-elipsis"><img src="/images/profile/gray-elipsis.png"/></button>
+					</div>
 				</div>
 			</div>
 		)
