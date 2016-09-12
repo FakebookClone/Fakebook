3c284b3fe03c76000e50c6023d9d21bf9998ae8b
mlecke <mlecke@beespace.com>
2016-09-12 17:42:52 -0600

    fixed home footer, add oldposts, started birthdaybox

--- public/app/components/profile/ProfileOldPosts.jsx
+++ public/app/components/profile/ProfileOldPosts.jsx
@@ -3,16 +3,20 @@ import React from 'react';
 require('../../../stylesheets/components/profile/ProfileOldPosts.scss');
 
 export default class ProfileOldPosts extends React.Component {
-  constructor () {
-    super()
-  }
+	constructor() {
+		super()
+	}
 
-  render () {
-    return (
-      <div>
-        <img src="/images/profile/posts-from-1990.png"/>
-        <p>Posts from 1990</p>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div className="main-old-posts-wrapper">
+				<div>
+					<img src="/images/profile/posts-from-1990.png"/>
+				</div>
+				<div>
+					<p className="posts-from-text">Posts from 1990</p>
+				</div>
+			</div>
+		)
+	}
 }
