60af2fea7fa3a9747ab74538d2e542b5408487a4
Brandon Unbedacht <unbedachtb@gmail.com>
2016-09-12 15:55:16 -0600

    made progress on the friend request box

--- public/app/components/find_friends/FindFriends.jsx
+++ public/app/components/find_friends/FindFriends.jsx
@@ -11,19 +11,33 @@ export default class FindFriends extends React.Component {
 
 	render() {
 		return (
-			<div>
-				<div>
-					<h1>Respond to Your Friend Request</h1>
-					<a href="#">View Sent Requests</a>
-				</div>
-				<div>
-					Friend Request Info Here
-				</div>
-				<div>
-					<button>Confirm</button>
-					<button>Delete Request</button>
+      <div>
+			<div className="main-wrapper">
+				<div className="main-column-wrapper">
+					<div className="wrapper-for-respond-to-request-box">
+						<div className="wrapper-for-top-of-request-box">
+								<h1>Respond to Your Friend Request</h1>
+							<div className="wrapper-for-link-near-top">
+								<a href="#">View Sent Requests</a>
+							</div>
+						</div>
+						<div className="wrapper-for-bottom-of-request-box">
+							<img src="../../../images/find_friends/james.jpg"></img>
+							<div className="wrapper-for-info-about-requesting-person">
+							  <a href="#">James Lemire</a> <br />
+								<img src="../../../images/find_friends/grad_cap.png"></img>
+								<p>Atlantic High School</p>
+								<p>Craig Walker is a mutual friend.</p>
+							</div>
+						</div>
+						<div className="wrapper-for-confirm-or-delete-of-requesting-person">
+							<button>Confirm</button>
+							<button>Delete Request</button>
+						</div>
+					</div>
 				</div>
 			</div>
+</div>
 		)
 	}
-}
+	}
