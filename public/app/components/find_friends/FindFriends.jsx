df70a0dc6eb82a0fbf0f55d60086d033ef99079c
Brandon Unbedacht <unbedachtb@gmail.com>
2016-09-12 19:19:48 -0600

    starting over the format

--- public/app/components/find_friends/FindFriends.jsx
+++ public/app/components/find_friends/FindFriends.jsx
@@ -7,35 +7,54 @@ require('../../../stylesheets/components/find_friends/FindFriends.scss');
 export default class FindFriends extends React.Component {
 	constructor() {
 		super()
+		this.state = {
+			user: null
+		}
 	}
-
+	componentWillMount() {
+		this.setState({
+			user: JSON.parse(localStorage.getItem('fakebook_user'))
+		});
+	}
 	render() {
 		return (
-			<div className="main-wrapper">
-				<div className="main-column-wrapper">
-					<div className="wrapper-for-respond-to-request-box">
-						<div className="wrapper-for-top-of-request-box">
-								<h1>Respond to Your Friend Request</h1>
-							<div className="wrapper-for-link-near-top">
-								<a href="#">View Sent Requests</a>
+			<div>
+				<GlobalHeader user={this.state.user} />
+				  <div className="main-wrapper">
+						<div className="respond-to-your-friend-request">
+							<div className="top-left-text-and-link">
+								<p>Respond to Your Friend Request</p>
+								<a href="#">View Send Requests</a>
 							</div>
-						</div>
-						<div className="wrapper-for-bottom-of-request-box">
-							<img src="../../../images/find_friends/james.jpg"></img>
-							<div className="wrapper-for-info-about-requesting-person">
-							  <a href="#">James Lemire</a> <br />
+							<div className="james-image">
+							  <img src = "../../../images/find_friends/james.jpg"></img>
+							</div>
+							<div className="text-about-james">
+								<a href="#">James Lemire</a>
 								<img src="../../../images/find_friends/grad_cap.png"></img>
 								<p>Atlantic High School</p>
-								<p>Craig Walker is a mutual friend.</p>
+								<p>Friends with Michael Lecke.</p>
+							</div>
+							<div className="wrapper-for-right-of-request-box">
+							  <div className="wrapper-for-confirm-button">
+								  <button className="confirm-button">Confirm</button>
+							  </div>
+							  <div className="wrapper-for-delete-request-button">
+								  <button className="delete-request-button">Delete Request</button>
+							  </div>
 							</div>
 						</div>
-						<div className="wrapper-for-confirm-or-delete-of-requesting-person">
-							<button className="confirm-button">Confirm</button>
-							<button className="delete-request-button">Delete Request</button>
+					  <div className="people-you-may-know">
+
+						</div>
+						<div className="add-personal-contacts">
+
+						</div>
+						<div className="search-for-friends">
+
 						</div>
 					</div>
-				</div>
 			</div>
 		)
 	}
-	}
+}
