8e0093c2abc5b1e7527165c3b2aef1b4f58a9e90
mlecke <mlecke@beespace.com>
2016-09-13 11:09:58 -0600

    britday box

--- public/app/components/profile/ProfileBirthdayBox.jsx
+++ public/app/components/profile/ProfileBirthdayBox.jsx
@@ -10,12 +10,16 @@ export default class ProfileBirthdayBox extends React.Component {
   render () {
     return (
       <div className="profile-birthday-box-wrapper">
-        <h1>Born on January 1, 1990</h1>
-        <img src="/images/profile/baby-blue-profile.png"/>
-        <img src="/images/profile/gray-people.png"/>
+      <div>
+      <h1>Born on January 1, 1990</h1>
+      </div>
+      <div>
+      <img src="/images/profile/baby-blue-profile.png"/>
+      </div>
+        {/* <img src="/images/profile/gray-people.png"/>
         <span className="gray"> · </span>
         <img src="/images/profile/gray-globe.png"/>
-        <i className="fa fa-angle-down gray"></i>
+        <i className="fa fa-angle-down gray"></i> */}
       </div>
     )
   }
