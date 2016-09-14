55fe74d1f54b6d8332199961d8f460bbd35472f3
Lonnie McGill <mcgizmo_26@yahoo.com>
2016-09-14 14:48:54 -0600

    merging

--- public/app/components/profile/ProfilePostStatus.jsx
+++ public/app/components/profile/ProfilePostStatus.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Axios from 'axios';
 
 require('../../../stylesheets/components/profile/ProfilePostStatus.scss');
+
 var imageshome = '/images/home/';
 var images = '/images/main/';
 
@@ -25,37 +26,57 @@ export default class ProfilePostStatus extends React.Component {
 	render() {
 		return (
 			<div className="profile-post-status-wrapper">
-			{this.state.dimmerVisible
-				? <div onClick={this.toggleDimmer.bind(this, false)} className="dimmer"></div>
-				: null
+				{this.state.dimmerVisible
+					? <div onClick={this.toggleDimmer.bind(this, false)} className="dimmer"></div>
+					: null
 }
-				<h1>
-					<span>
-						<img src="/images/profile/status-pencil.png"/>
-					</span>
-					<p>Status</p>
-					<span>
-						<img src="/images/profile/photo-video.png"/>
-					</span>
-					<p>Photo / Video</p>
-					<span>
-						<img src="/images/profile/flag-blue-profile.png"/>
-					</span>
-					<p>Life Event</p>
-					{this.state.closeVisible
-						? <div onClick={this.toggleDimmer.bind(this, true)} className="closeDiv"><img src={imageshome + 'gray-x.png'}/></div>
-						: null
-					}
-				</h1>
-				<div className="mid-post-profile">
-					<div>
-						<img src={this.props.user.profile_pic}/>
-					</div>
+				<div className="profile-center-post-container">
+					<h1>
+						<span>
+							<img src="/images/profile/status-pencil.png"/>
+						</span>
+						<p>Status</p>
+						<span>
+							<img src="/images/profile/photo-video.png"/>
+						</span>
+						<p>Photo / Video</p>
+						<span>
+							<img src="/images/profile/flag-blue-profile.png"/>
+						</span>
+						<p>Life Event</p>
+
+						{this.state.closeVisible
+							? <div onClick={this.toggleDimmer.bind(this, true)} className="profileCloseDiv"><img src={imageshome + 'gray-x.png'}/></div>
+							: null
+}
+					</h1>
+					<div className="mid-post-profile">
+						<div>
+							<img src={this.props.user.profile_pic}/>
+						</div>
 						<textarea onClick={this.toggleDimmer.bind(this)} placeholder="What's on your mind?" className="profile-post-textarea" onChange={this.postCatcher.bind(this)} value={this.state.post}/>
-				</div>
-				<div className="lower-profile-button-container">
-					<button className="fb-bttn"><img src={images + 'friendsbttn.png'}/></button>
-					<button className="post-bttn" onClick={this.post.bind(this)}>Post</button>
+					</div>
+					<div className="lower-profile-container-bottom">
+
+						{this.state.iconVisible === true
+							? <div className="lower-profile-icon-container">
+									<div>
+										<img src={imageshome + 'tag-people.png'}/>
+									</div>
+									<div>
+										<img src={imageshome + 'smiley2.png'}/>
+									</div>
+									<div>
+										<img src={imageshome + 'check-in2.png'}/>
+									</div>
+								</div>
+							: null
+}
+						<div className="lower-profile-button-container">
+							<button className="fb-bttn"><img src={images + 'friendsbttn.png'}/></button>
+							<button className="post-bttn" onClick={this.post.bind(this)}>Post</button>
+						</div>
+					</div>
 				</div>
 			</div>
 		)
@@ -129,24 +150,22 @@ export default class ProfilePostStatus extends React.Component {
 	}
 
 	toggleDimmer(override) {
-		console.log('TOGGLE DIMMER HIT');
-		this.setState({ dimmerVisible: true })
-		// if (this.state.post == "" && !(this.state.file)) {
-		// 	this.setState({
-		// 		dimmerVisible: !this.state.dimmerVisible,
-		// 		closeVisible: !this.state.closeVisible,
-		// 		iconVisible: true
-		// 	})
-		// } else if (this.state.post !== "") {
-		// 	this.setState({dimmerVisible: true, closeVisible: true});
-		// }
-		//
-		// if (override) {
-		// 	this.setState({
-		// 		dimmerVisible: !this.state.dimmerVisible,
-		// 		closeVisible: !this.state.closeVisible
-		// 	})
-		// }
+		if (this.state.post == "" && !(this.state.file)) {
+			this.setState({
+				dimmerVisible: !this.state.dimmerVisible,
+				closeVisible: !this.state.closeVisible,
+				iconVisible: true
+			})
+		} else if (this.state.post !== "") {
+			this.setState({dimmerVisible: true, closeVisible: true});
+		}
+
+		if (override) {
+			this.setState({
+				dimmerVisible: !this.state.dimmerVisible,
+				closeVisible: !this.state.closeVisible
+			})
+		}
 	}
 
 }
