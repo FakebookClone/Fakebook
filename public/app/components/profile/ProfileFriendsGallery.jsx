9e6389a90b6de703758c35065c976f66012b041e
Brandon Unbedacht <unbedachtb@gmail.com>
2016-09-16 17:58:10 -0600

    made changes to photo and friends tabs on profile page

--- public/app/components/profile/ProfileFriendsGallery.jsx
+++ public/app/components/profile/ProfileFriendsGallery.jsx
@@ -10,19 +10,19 @@ export default class ProfileFriendsGallery extends React.Component {
 	render() {
 		return (
 			<div className="main-container">
-			  <div className="upper-photos-section-wrapper">
+			  <div className="upper-friends-section-wrapper">
 			    <div className="friends-image-wrapper">
 			      <img src="/images/profile/friends_24.png"/><h1>Friends</h1>
 			    </div>
 
 			    <div className="create-requests-find-and-edit-buttons">
-			      <button className="friend-requests-button"><p className="text-friend-requests">Friend Requests</p> <span className="red-counter">1</span></button>
+			      {/* <button className="friend-requests-button"><p className="text-friend-requests">Friend Requests</p> <span className="red-counter">1</span></button> */}
 			      <button className="find-friends-button"><img className="gray-plus-image" src="/images/profile/gray-plus.png"/>Find Friends</button>
 			      <button className="edit-privacy-button">Edit Privacy</button>
 			    </div>
 			  </div>
 			  <div className="lower-section-wrapper">
-			    <h1 className="no-friends-to-show">No friends to Show</h1>
+			    <h1 className="no-friends-to-show">No friends to show</h1>
 			  </div>
 			</div>
 		)
--- public/app/components/profile/ProfilePhotosGallery.jsx
+++ public/app/components/profile/ProfilePhotosGallery.jsx
@@ -14,6 +14,12 @@ export default class ProfilePhotosGallery extends React.Component {
 					<div className="photo-image-wrapper">
 						<img src="/images/profile/photos_24.png"/>
 						<h1>Photos</h1>
+						<div className="parent-of-albums-p">
+							<p>Albums</p>
+							<div className="white-arrow-image-photos">
+								<img src="/images/profile/white-arrow3.png" />
+							</div>
+						</div>
 					</div>
 
 					<div className="create-add-video-and-pencil-buttons">
@@ -24,7 +30,23 @@ export default class ProfilePhotosGallery extends React.Component {
 					</div>
 				</div>
 				<div className="lower-section-wrapper">
-					<h1 className="no-photos-to-show">No Photos to Show</h1>
+					{/* <h1 className="no-photos-to-show">No Photos to Show</h1> */}
+					<div className="dotted-line-box">
+						<div className="plus-and-create-album">
+							<img src="/images/profile/gray-plus.png" />
+							<p>Create Album</p>
+						</div>
+					</div>
+					<div className="timeline-photos-boxes">
+					<div className="timeline-photos-box">
+						<img src="/images/profile/empty-album.png" />
+						</div>
+						<div className="timeline-photos-box2">
+							<a href="#"><p>Timeline Photos</p></a>
+							<p>Empty</p>
+							<img src="/images/profile/gears-light-grey.png" />
+						</div>
+          </div>
 				</div>
 			</div>
 		)
