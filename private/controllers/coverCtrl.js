4ed8fa9ea866a916c796a9c7ac175fcab902dff4
Michael Lecke <mlecke@beespace.com>
2016-09-17 20:55:23 -0600

    add profilepic large

--- private/controllers/coverCtrl.js
+++ private/controllers/coverCtrl.js
@@ -8,5 +8,14 @@ module.exports = {
       console.log('coverCtrl', r);
       res.json(r);
     });
+  },
+
+  getProfilePic: function(req, res, next) {
+    console.log("req params", req.params);
+    //db call
+    db.cover.getProfilePic([req.params.facebook_id], function(err, r) {
+      console.log("coverCtrl", r);
+      res.json(r);
+    })
   }
 };
--- public/app/components/profile/ProfileAddPhoto.jsx
+++ public/app/components/profile/ProfileAddPhoto.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import Axios from 'axios';
 
 require('../../../stylesheets/components/profile/ProfileAddPhoto.scss');
 
 export default class ProfilePhoto extends React.Component {
-	constructor() {
-		super()
+	constructor(props) {
+		super(props)
+		this.state = { profilePic: null };
+	}
+
+	componentWillMount() {
+		console.log(this.props.user);
+		Axios.get(`/api/profile-pic-lg/${this.props.user}`).then(r => {
+			console.log("response profilepic", r);
+			this.setState({ profilePic: r.data[0].profile_pic_lg });
+		});
 	}
 
 	render() {
@@ -12,8 +22,9 @@ export default class ProfilePhoto extends React.Component {
 			<div className="add-photo-wrapper">
 
 				<div className="silhouette-add-photo">
-					<img className="profile-profile-picture" src={this.props.user.profile_pic} />
+					<img className="profile-profile-picture" src={this.state.profilePic} />
 					<div className="add-photo-opaque">
+					<input type="file" accept="image/*" onChange={this.addPhoto.bind(this)} className="add-profile-pic" />
 						<div className="camera-pic">
 							<img src="/images/profile/white-camera.png"/>
 						</div>
@@ -23,5 +34,24 @@ export default class ProfilePhoto extends React.Component {
 
 			</div>
 		)
+	}
+
+	addPhoto(e) {
+		const reader = new FileReader();
+		const file = e.target.files[0];
+		var fileUpload = {};
+		reader.onload = (upload) => {
+			fileUpload = {
+				imageBody: upload.target.result,
+				imageName: file.name,
+				imageExtension: file.type,
+				userEmail: this.props.user.email
+			}
+			Axios.post(`/api/aws/upload/${this.props.user.facebook_id}`, {file: fileUpload}).then(r => {
+				console.log("data", r);
+				this.setState({ profilePic: r.data });
+			});
+		}
+		reader.readAsDataURL(file);
 	}
 }
