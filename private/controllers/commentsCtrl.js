f272c96e4e3c7aec8528aabe22df6cbbc942f443
James Lemire <webdev.jameslemire@gmail.com>
2016-09-18 14:47:38 -0600

    Hide and unhide comments based on profile_id, sql

--- private/controllers/commentsCtrl.js
+++ private/controllers/commentsCtrl.js
@@ -13,8 +13,15 @@ module.exports = {
     })
   },
   hideComment: function(req, res) {
+    db.comments.hideComment([req.body.comment_id, req.body.profile_id], function(req, res) {
+      db.comments.getComments([req.body.post_id], function(err, r) {
+        res.json(r);
+      })
+    })
+  }
+  unhideComment: function(req, res) {
     console.log('HIDE COMMENT HIT', req.body);
-    db.comments.unhideComment([req.body.comment_id], function(err, r) {
+    db.comments.unhideComment([req.body.comment_id, req.body.profile_id], function(err, r) {
       db.comments.getComments([req.body.post_id], function(err, r) {
         res.json(r);
       })
